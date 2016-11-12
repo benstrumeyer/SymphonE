591a4f3af57e22da241b4deaa86ee49c65f2658d
cypherix93 <bikram.ghosh93@gmail.com>
2016-11-12 12:33:51 -0500

    Added layout styles and made it responsive.

--- src/client/scripts/angular/core/helpers/RoutingHelper.ts
+++ src/client/scripts/angular/core/helpers/RoutingHelper.ts
@@ -6,8 +6,6 @@ export function registerRoute(name: string, state: IState)
 {
     AngularApp.config(function ($stateProvider: IStateProvider)
     {
-        console.log(name, state);
-
         $stateProvider.state(name, state);
     });
 }
