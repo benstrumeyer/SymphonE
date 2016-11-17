623f92ea735485f75a9d6504a1f1d36365b6e0cb
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2016-11-16 21:09:17 -0500

    Created chat-navbar. Will switch to tabs.

---
+++ src/client/scripts/angular/app/chat/ChatNavBar.component.ts
@@ -0,0 +1,10 @@
+import {AngularApp} from "../../AngularApp";
+
+class ChatNavBarController{
+
+}
+
+AngularApp.component("chatNavBar", {
+    controller: ChatNavBarController,
+    template: require("./ChatNavBar.template.html")
+});
