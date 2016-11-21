027b741beab444783bb187194bd8ee80554b63ef
cypherix93 <bikram.ghosh93@gmail.com>
2016-11-20 22:38:09 -0500

    Can properly load the scripts now. Need a way to load the html files gracefully.

--- src/client/scripts/angular/AngularApp.ts
+++ src/client/scripts/angular/AngularApp.ts
@@ -1,3 +1,6 @@
+import glob = require("glob");
+import path = require("path");
+
 import * as angular from "angular";
 
 // Init the angular module
@@ -10,8 +13,13 @@ angular.module("AngularApp", [
     "toastr"
 ]);
 
-// Global Angular App Declaration
-export const AngularApp = angular.module("AngularApp");
+// Glob require all the angular app files
+var coreFiles = glob.sync(path.join(__dirname, "./core/**/*.js"));
+var appFiles = glob.sync(path.join(__dirname, "./app/**/*.js"));
+coreFiles.concat(appFiles).forEach(path => require(path));
 
 console.log("LOOK MA! I WORK FROM ANGULAR!");
+
+// Global Angular App Declaration
+export const AngularApp = angular.module("AngularApp");
 
